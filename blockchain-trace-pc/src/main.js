911da739a7e4e6279e72b7753fb5f353673f8aa0
chainer2021 <chainer2021@163.com>
2023-01-10 17:27:39 +0800

    dev deploy

--- blockchain-trace-pc/src/main.js
+++ blockchain-trace-pc/src/main.js
@@ -1,76 +1,84 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
-import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import "normalize.css/normalize.css"; // a modern alternative to CSS resets
 
-import Element from 'element-ui'
-import './assets/styles/element-variables.scss'
+import Element from "element-ui";
+import "./assets/styles/element-variables.scss";
 
-import '@/assets/styles/index.scss' // global css
-import '@/assets/styles/ruoyi.scss' // ruoyi css
-import App from './App'
-import store from './store'
-import router from './router'
-import permission from './directive/permission'
+import "@/assets/styles/index.scss"; // global css
+import "@/assets/styles/ruoyi.scss"; // ruoyi css
+import App from "./App";
+import store from "./store";
+import router from "./router";
+import permission from "./directive/permission";
 
-import './assets/icons' // icon
-import './permission' // permission control
+import "./assets/icons"; // icon
+import "./permission"; // permission control
 import { getDicts } from "@/api/system/dict/data";
 import { getConfigKey } from "@/api/system/config";
-import { parseTime, resetForm, addDateRange, selectDictLabel, selectDictLabels, download, handleTree } from "@/utils/ruoyi";
+import {
+  parseTime,
+  resetForm,
+  addDateRange,
+  selectDictLabel,
+  selectDictLabels,
+  download,
+  handleTree,
+} from "@/utils/ruoyi";
 import Pagination from "@/components/Pagination";
 //自定义表格工具扩展
-import RightToolbar from "@/components/RightToolbar"
-import axios from 'axios'
+import RightToolbar from "@/components/RightToolbar";
+import axios from "axios";
 import SnowflakeId from "snowflake-id";
 
-import JsonViewer from 'vue-json-viewer'
- 
+import JsonViewer from "vue-json-viewer";
+
 // Import JsonViewer as a Vue.js plugin
-Vue.use(JsonViewer)
+Vue.use(JsonViewer);
 
 // 全局方法挂载
-Vue.prototype.getDicts = getDicts
-Vue.prototype.getConfigKey = getConfigKey
-Vue.prototype.parseTime = parseTime
-Vue.prototype.resetForm = resetForm
-Vue.prototype.addDateRange = addDateRange
-Vue.prototype.selectDictLabel = selectDictLabel
-Vue.prototype.selectDictLabels = selectDictLabels
-Vue.prototype.download = download
-Vue.prototype.handleTree = handleTree
+Vue.prototype.getDicts = getDicts;
+Vue.prototype.getConfigKey = getConfigKey;
+Vue.prototype.parseTime = parseTime;
+Vue.prototype.resetForm = resetForm;
+Vue.prototype.addDateRange = addDateRange;
+Vue.prototype.selectDictLabel = selectDictLabel;
+Vue.prototype.selectDictLabels = selectDictLabels;
+Vue.prototype.download = download;
+Vue.prototype.handleTree = handleTree;
 
 Vue.prototype.msgSuccess = function (msg) {
   this.$message({ showClose: true, message: msg, type: "success" });
-}
+};
 
 Vue.prototype.msgError = function (msg) {
   this.$message({ showClose: true, message: msg, type: "error" });
-}
+};
 
 Vue.prototype.msgInfo = function (msg) {
   this.$message.info(msg);
-}
+};
 
 Vue.prototype.$httpBlock = axios;
-Vue.prototype.$httpUrl = "http://localhost:8080/route";
+Vue.prototype.$httpUrl = "http://43.133.36.116:8083/route";
 Vue.prototype.$snowFlakeId = SnowflakeId;
 Vue.prototype.$store = store;
 // 全局组件挂载
-Vue.component('Pagination', Pagination)
-Vue.component('RightToolbar', RightToolbar)
+Vue.component("Pagination", Pagination);
+Vue.component("RightToolbar", RightToolbar);
 
-Vue.use(permission)
+Vue.use(permission);
 
 //引入地图插件
-import VueAMap from 'vue-amap';
+import VueAMap from "vue-amap";
 Vue.use(VueAMap);
- 
+
 //高德地图方法
 VueAMap.initAMapApiLoader({
-　　key: '3e5db00ff818adf2b2e2f600f0a72608',
-　　　v: '1.4.4'
+  key: "3e5db00ff818adf2b2e2f600f0a72608",
+  v: "1.4.4",
 });
 
 /**
@@ -83,14 +91,14 @@ VueAMap.initAMapApiLoader({
  */
 
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium' // set element-ui default size
-})
+  size: Cookies.get("size") || "medium", // set element-ui default size
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  render: h => h(App)
-})
+  render: (h) => h(App),
+});
